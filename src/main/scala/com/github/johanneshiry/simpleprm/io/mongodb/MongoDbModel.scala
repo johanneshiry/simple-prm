faee1f994e33577dc68e1710d638a15cbf7bb647
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2022-03-27 22:43:09 +0200

    sanity check in MongoDbModel

--- src/main/scala/com/github/johanneshiry/simpleprm/io/mongodb/MongoDbModel.scala
+++ src/main/scala/com/github/johanneshiry/simpleprm/io/mongodb/MongoDbModel.scala
@@ -8,17 +8,54 @@ import com.github.johanneshiry.simpleprm.io.model.StayInTouch
 import ezvcard.VCard
 import ezvcard.property.Uid
 
+import scala.util.{Failure, Success, Try}
+
 private[mongodb] object MongoDbModel {
 
-  final case class Contact(
+  final case class Contact private (
       _id: Uid,
       vCard: VCard,
       stayInTouch: Option[StayInTouch]
   )
 
   object Contact {
-    def apply(vCard: VCard, stayInTouch: Option[StayInTouch]): Contact =
-      Contact(vCard.getUid, vCard, stayInTouch)
+    def apply(vCard: VCard, stayInTouch: Option[StayInTouch]): Try[Contact] =
+      checkAndBuild(vCard.getUid, vCard, stayInTouch)
+
+    private def checkAndBuild(
+        _id: Uid,
+        vCard: VCard,
+        stayInTouch: Option[StayInTouch]
+    ): Try[Contact] = {
+
+      def allUidEqual(
+          _id: Uid,
+          vCardUid: Uid,
+          stayInTouchUid: Option[Uid] = None
+      ): Boolean =
+        _id.getValue.equals(vCardUid.getValue) && _id.getValue.equals(
+          stayInTouchUid.getOrElse(_id).getValue
+        )
+
+      stayInTouch match {
+        case Some(stayInTouch)
+            if allUidEqual(_id, vCard.getUid, Some(stayInTouch.contactId)) =>
+          Success(Contact(_id, vCard, Some(stayInTouch)))
+        case None if allUidEqual(_id, vCard.getUid) =>
+          Success(Contact(_id, vCard, stayInTouch))
+        case _ =>
+          Failure(
+            new IllegalArgumentException(
+              s"Cannot construct Contact instance with different Uids.\n" +
+                s"_id = ${_id.getValue}\n" +
+                s"vCardUid = ${vCard.getUid.getValue}" +
+                s"${if (stayInTouch.isDefined) "\nstayInTouchContactId: " + stayInTouch.get.contactId.getValue
+                else ""}"
+            )
+          )
+      }
+
+    }
 
   }
 
