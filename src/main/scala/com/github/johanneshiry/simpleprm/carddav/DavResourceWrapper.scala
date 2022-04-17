7f9b765382dece831699de5a310ea9a82bf9f7f8
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2022-04-17 17:11:44 +0200

    let DavResourceWrapper respect ports in full path if provided

--- src/main/scala/com/github/johanneshiry/simpleprm/carddav/DavResourceWrapper.scala
+++ src/main/scala/com/github/johanneshiry/simpleprm/carddav/DavResourceWrapper.scala
@@ -11,6 +11,15 @@ import java.net.URI
 final case class DavResourceWrapper(davResource: DavResource, serverUri: URI) {
 
   def fullPath: String =
-    s"${serverUri.toURL.getProtocol}://${serverUri.getHost + davResource.getHref.getPath}"
+    s"$protocol$host$maybePort$resource"
+
+  private def protocol = s"${serverUri.toURL.getProtocol}://"
+
+  private def host = serverUri.getHost
+
+  private def resource = davResource.getHref.getPath
+
+  private def maybePort =
+    if (serverUri.getPort != -1) s":${serverUri.getPort}" else ""
 
 }
