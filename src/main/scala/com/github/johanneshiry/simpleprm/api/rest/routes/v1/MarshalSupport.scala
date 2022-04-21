adefa07fa40348e955510d026197d9546b22656f
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2022-04-21 21:14:33 +0200

    improved rest api MarshalSupport

--- src/main/scala/com/github/johanneshiry/simpleprm/api/rest/routes/v1/MarshalSupport.scala
+++ src/main/scala/com/github/johanneshiry/simpleprm/api/rest/routes/v1/MarshalSupport.scala
@@ -6,7 +6,14 @@ package com.github.johanneshiry.simpleprm.api.rest.routes.v1
 
 import akka.http.scaladsl.marshalling
 import akka.http.scaladsl.marshalling.{Marshal, Marshaller, Marshalling}
-import akka.http.scaladsl.model.{HttpResponse, ResponseEntity, StatusCode}
+import akka.http.scaladsl.model.{
+  ContentTypes,
+  HttpEntity,
+  HttpResponse,
+  ResponseEntity,
+  StatusCode
+}
+import akka.util.ByteString
 
 import scala.concurrent.{ExecutionContext, Future}
 import io.circe.generic.auto.*
@@ -15,6 +22,11 @@ import io.circe.syntax.*
 import scala.language.postfixOps
 
 trait MarshalSupport {
+
+  val emptyHttpJsonResponse: HttpEntity.Strict = HttpEntity.Strict(
+    ContentTypes.`application/json`,
+    data = ByteString.fromString("{}")
+  )
 
   def marshal[T](value: T, statusCode: StatusCode)(implicit
       marshaller: marshalling.Marshaller[
@@ -28,5 +40,19 @@ trait MarshalSupport {
         HttpResponse(statusCode, entity = entity)
       } :: Nil
     }
+
+  def marshal(
+      statusCode: StatusCode,
+      entity: ResponseEntity = emptyHttpJsonResponse
+  )(implicit
+      ec: ExecutionContext
+  ): Future[List[Marshalling.Opaque[HttpResponse]]] =
+    Future(
+      List(
+        Marshalling.Opaque { () =>
+          HttpResponse(statusCode, entity = entity)
+        }
+      )
+    )
 
 }
